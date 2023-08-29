0a730fec6a70c13ee6536cbf44ac56d1f06a5a03
mayyzena012 <143430528+mayyzena012@users.noreply.github.com>
2023-08-28 22:20:41 -0300

    Update runy.py

--- runy.py
+++ runy.py
@@ -29,3 +29,57 @@ rd = n1 % n2
 
 
 print("a soma e {} a multi {} a sub {} a poten {} a divis {} a divisao inteira e {} resto da div {}".format(s,m,s1,p,d,di,rd))
+
+n =int(input("digite um numero"))
+
+d = n * 2
+t = n * 3
+r = n **(1/2)
+
+print("o dobro deste numero e {} o triplo deste numero e {} e sua raiz e {}".format(d, t, r))
+
+
+n1 =int(input("nota numero 1"))
+n2 =int(input("nota numero 2"))
+
+m = n1 + n2 / 2
+
+print("a media da soma das notas e {}".format(m))
+
+p = int(input("digite um numero"))
+
+t0 = p * 1
+t1 = p * 2
+t2 = p * 3
+t3 = p * 4
+t4 = p * 5
+t5 = p * 6
+t6 = p * 7
+t7 = p * 8
+t8 = p * 9
+t9 = p * 10
+
+print("a tabuada de {} e \n {} \n {} \n {} \n {} \n {} \n {} \n {} \n {} \n {} \n {}".format(p, t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, end='')) 
+
+num =int(input("digite um numero"))
+
+c = num / 100
+
+print('{} quilometros em centimentros e {}'.format(num, c))
+
+lar =int(input('qual largura'))
+alt =int(input('qual altura'))
+
+a = alt * lar
+t = a * 50
+
+print('uma parede com largura de {} e altura de {} tem {} metros quadrados'.format(lar, alt, a))
+print('sera necessario {}'.format(t))
+
+preco =int(input('qual preco'))
+
+d = 5 * preco 
+d1 = d / 100
+
+print('este produto custando {} tera o valor {} com 5% de desconto'.format(preco, d1))
+
