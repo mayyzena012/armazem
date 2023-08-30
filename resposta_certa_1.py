aaf8f7a61bb324149dda4c4477c81b8d6886a188
Mayra <143430528+mayyzena012@users.noreply.github.com>
2023-08-30 14:41:54 -0300

    Update resposta_certa_1.py

--- resposta_certa_1.py
+++ resposta_certa_1.py
@@ -13,16 +13,29 @@ print('-=-'*40)
 resposta = str(input('Você me ama?')).strip().lower()
 
 if 'sim' in resposta:
-    print(f'PROCESSANDO...')
+    print('PROCESSANDO...')
     sleep(5)
     print('Parabens sua resposta esta correta e eu também te amo, amor')
 else:
     'nao' and 'não' in resposta
     print('VOCÊ É MALUCA?????')
-    print(f'CUIDADO COM SUAS RESPOSTAS... E TOME')
+    print('CUIDADO COM SUAS RESPOSTAS... E TOME')
     sleep(5)
-    print(f'seu sistema será desligado como punição')
-    sleep(5)
-    #os.system("shutdown /s /t 1")
-    
+    print('seu sistema será desligado como punição')
+    sleep(19)
+    os.system('shutdown /s /t 1')
 
+resposta2 = input('você vai desinstalar o Valorant?')
+
+if 'sim' in resposta2:
+    print('Ok, então, valorante será destruido do seu PC')
+    print('PROCESSANDO...')
+    sleep(5)
+    input('Obrigada por participar amor, digite algo para sair do programa :D')
+else:
+    'nao' and 'não' in resposta2
+    print(f'COMO ASSIM? VOCE VAI DIZER {resposta2}??')
+    print('pois você sera punida agora!')
+    print('seu sistema sera desligado em punição! E TOME!')
+    sleep(10)
+    os.system('shutdown /s /t 1')
