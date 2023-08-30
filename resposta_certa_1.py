e8297376df03ec35c2b90ad61db29123b5f972b7
Mayra <143430528+mayyzena012@users.noreply.github.com>
2023-08-30 10:22:10 -0300

    Create resposta_certa_1.py

---
+++ resposta_certa_1.py
@@ -0,0 +1,27 @@
+#bem vindo ao jogo da resposta certa
+
+import os
+from time import sleep
+
+print('-=-'*30)
+print('Olá, Ana! Seja bem vinda a um teste extremamente dificil sobre nossa relação!')
+print('-='*40)
+print('Este teste é perigoso e você deve responder ele com cuidado')
+print('Responda com atenção as próximas perguntas!!')
+print('-=-'*40)
+
+resposta = str(input('Você me ama?')).strip().lower()
+
+if 'sim' in resposta:
+    print(f'PROCESSANDO...')
+    sleep(5)
+    print('Parabens sua resposta esta correta e eu também te amo, amor')
+elif 'nao' and 'não' in resposta:
+    print('VOCÊ É MALUCA?????')
+    print(f'CUIDADO COM SUAS RESPOSTAS... E TOME')
+    sleep(5)
+    print(f'seu sistema será desligado como punição')
+    sleep(5)
+    #os.system("shutdown /s /t 1")
+    
+
