20707395b1f7f5e13554a53693069f00b5fc2941
mayyzena012 <143430528+mayyzena012@users.noreply.github.com>
2023-09-05 20:51:08 -0300

    Update ex069.py

--- ex069.py
+++ ex069.py
@@ -3,20 +3,22 @@ cont = 0
 fem = 0
 masc = 0
 ida = 0
-SEXO_MASCULINO = ('homem', 'm', 'masculino', 'h')
-SEXO_FEMININO = ('mulher', 'feminino', 'f')
 while True:
+    SEXO_MASCULINO = ('homem', 'm', 'masculino', 'h')
+    SEXO_FEMININO = ('mulher', 'feminino', 'f')
     idade = int(input('Digite sua idade '))
     sexo = input('Digite seu sexo ').lower()
-    pergunta = input('Voce quer continuar S/N? ').upper()
     cont += 1
+    pergunta = input('Voce quer continuar S/N? ').upper()
     if sexo in SEXO_MASCULINO:
         masc += 1
-    if idade == 18:
+    elif idade == 18:
         ida += 1
-    if sexo in SEXO_FEMININO:
+    elif sexo in SEXO_FEMININO:
         if idade<20:
             fem += 1
+    else:
+        print('resposta invalida')
     if pergunta == 'N':
         print('Programa encerrado.')
         print(f'Voce digitou {cont} pessoas. {masc} sao homens, {ida} tem 18 anos e {fem} mulheres tem menos de 20 anos')
