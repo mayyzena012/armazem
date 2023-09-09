bef6a314b00535da23392926129f9a9106b9b671
mayyzena012 <143430528+mayyzena012@users.noreply.github.com>
2023-09-08 21:36:35 -0300

    .

--- ex076.py
+++ ex076.py
@@ -6,7 +6,7 @@ prod = ('Microondas', 1200.95, 'Televisao', 2530.30,'Videogame', 4500.99, 'Sapat
 print('-'*50)
 print('{:^50}'.format('LISTA DE COMPRAS'))
 print('-'*50)
-print(f'{prod[0]}.....................................R${prod[1]}')
+print(f'{prod[0]:.<30}R${prod[1]}')
 print(f'{prod[2]}.....................................R${prod[3]}')
 print(f'{prod[4]}.....................................R${prod[5]}')
 print(f'{prod[6]}.....................................R${prod[7]}')
@@ -16,5 +16,8 @@ print(f'{prod[8]}.....................................R${prod[9]}')
 p = ('SABONETE','CIRURGIAO','MAIZENA','FUTEBOL','ENTORPECENTE','GIGATONICO','PROGRAMADOR','CONVERSAR','ESTUDANDO')
 print(p)
 
-if 'AEIOU' in p[1]:
-    print(p[1])
+for a in p:
+    print(f'na palavra {a} temos')
+    for letra in a:
+        if letra.lower() in 'aeiou':
+            print(letra, end= ' ')
