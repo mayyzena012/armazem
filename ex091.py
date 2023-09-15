d08fedab58e2afcc900abd68ce0c085106b281e6
mayyzena012 <143430528+mayyzena012@users.noreply.github.com>
2023-09-14 21:50:20 -0300

    .

--- ex091.py
+++ ex091.py
@@ -4,15 +4,13 @@ from random import randint
 jogadores = dict()
 
 
-jogadores['jogador 1'] = randint(1,6)
-jogadores['jogador 2'] = randint(1,6)
-jogadores['jogador 3'] = randint(1,6)
-jogadores['jogador 4'] = randint(1,6)
+for j in range(1,6):
+    jogadores[f'jogador{j}'] = randint(1,6)
 
 for k,v in jogadores.items():
     print(f'{k} tirou no dado o valor [ {v} ]')
 
-for i in sorted(jogadores, key=jogadores.get):
+for i in sorted(jogadores, key=jogadores.get, reverse=True):
     print(f'{i} tirou {jogadores[i]}')
 print(jogadores)
 
--- ex092.py
+++ ex092.py
@@ -1,9 +1,21 @@
+#35 anos de contribuicao
 
-from random import *
+cadastro = dict()
 
-jogadores = dict()
+print('-='*25)
+print('{:^50}'.format('CADASTRO PORTAL DO TRABALHADOR'))
+print('-='*25)
+cadastro['nome'] = str(input('Nome: '))
+nasc = int(input('Ano Nascimento: '))
+idade = 2023 - nasc
+cadastro['idade'] = idade
 
-for a in range(4):
-    jogadores['jogador'] = randint(1,6)
+cadastro['carteira'] = int(input('N Carteira de Trabalho: '))
+if cadastro['carteira'] != 0:
+    cadastro['contrato'] = int(input('Ano de contratacao: '))
+    cadastro['salario'] = int(input('Salario: '))
+else:
+    print('sabonte')
 
-print(jogadores)
+con = 2023 - cadastro['contrato'] 
+print(cadastro)
