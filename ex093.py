992dce7c6f2790d72836609937b363b48f967430
mayyzena012 <143430528+mayyzena012@users.noreply.github.com>
2023-09-17 17:47:52 -0300

    .

--- ex093.py
+++ ex093.py
@@ -7,11 +7,20 @@ jogador['nome'] = input('Nome do jogador: ')
 partidas = int(input('N de partidas jogadas: '))
 jogador['partidas'] = partidas
 
-
+total = 0
 for n in range(partidas):
     gols.append(int(input(f'quantos gols na partida {n}? ')))
     jogador['gols'] = gols
 
+for gol in gols:
+    total += gol
+    jogador['total_gols'] = total
 
-print(jogador)
-print(gols)
+print('-='*20)
+print(f'Jogador {jogador["nome"]} jogou {partidas} partidas!')
+
+print('-='*20)
+for p,g in enumerate(gols):
+    print(f'Marcando na partida {p}, fez {g} gols!')
+print(f'Marcando um total de {total} gols!')
+
---
+++ ex094.py
@@ -0,0 +1,7 @@
+#aqui voce coloca sua mae
+
+while True:
+    nome = input('Digite seu nome: ')
+    sexo = input('Digite seu sexo: ')
+    idade = int(input('Digite sua idade: '))
+
