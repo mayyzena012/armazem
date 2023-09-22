a549ba6505c980cfdcb1477dbc9d7c8531ab67d8
mayyzena012 <143430528+mayyzena012@users.noreply.github.com>
2023-09-22 19:14:37 -0300

    .

--- teste.py
+++ teste.py
@@ -1,10 +1,9 @@
-def calcular_area(comprimento, largura):
-    area = comprimento * largura
 
+pergunta = input('Escolha um carro [Civic, Palio, Ka, Duster, Gol e Saveiro] ').upper()
 
-comprimento_terreno = float(input("Digite o comprimento do terreno em metros: "))
-largura_terreno = float(input("Digite a largura do terreno em metros: "))
-
-resultado = calcular_area(comprimento_terreno, largura_terreno)
-
-print(f"A área do terreno é {resultado} metros quadrados.")
+match pergunta:
+    case 'Civic':
+       print('saboente')
+    case 'Palio':
+        print('Sanonete')
+       
