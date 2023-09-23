4de7eaf2872d557d6889c122ade98e6405114d9d
mayyzena012 <143430528+mayyzena012@users.noreply.github.com>
2023-09-23 17:09:42 -0300

    .

---
+++ c# teste/teste.cs
@@ -0,0 +1,25 @@
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        {
+
+            static void Main(string[] args)
+            
+            int b, a;
+            Console.Write("escreva a base: ");
+            b = int.Parse(Console.Readline());
+
+            Console.Write("Escreva a altura: ");
+            a = int.Parse(Console.Readline());
+
+            string area = b * a;
+
+            Console.Write(area)
+
+
+    }
+    }
+}
