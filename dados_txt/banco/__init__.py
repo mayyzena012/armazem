6b9c31f1795ab8754d782ae30a84670d6dcd29ea
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2023-09-28 22:34:37 -0300

    .

--- dados_txt/banco/__init__.py
+++ dados_txt/banco/__init__.py
@@ -1,16 +1,48 @@
+arq = "dados_txt/banco/cadastro.txt"
 
-#arquivo = open("cadastro","x")
-def abrir():
+def linha(tam = 40):
+    return "~" * tam
+
+def cabeca(txt):
+    print(linha())
+    print(txt.center(40))
+    print(linha())
+
+def leiaInt(enter):
+    while True:
+        try:
+            entrada = int(input(enter))
+            return entrada
+        except ValueError:
+            print("ERRO. Insira um valor valido")
+        except KeyboardInterrupt:
+            print("\n O usuario não escolheu nada")
+            return 4
+        
+
+def criarArquivo():
     try:
-        arquivo = open("cadastro.txt","rt")
+        arquivo = open(arq,"wt+")
+        print("Arquivo criado com sucesso")
+        arquivo.close()
     except:
-        print("erro ao abrir o arquivo")
-    else:
-        return print(arquivo.read())
+        print("Houve um erro ao criar o arquivo")
 
-def nome(name):
-    pass
+def lerArquivo():
+    try: 
+        arquivo = open(arq, "rt")
+        cabeca("PESSOAS CADASTRADAS")
+        lista = arquivo.read()
+        return lista
+    except:
+        print("ERRO ao abrir o arquivo")
 
-def idade(age):
-    pass
+def opcoes(lista):
+    for posicao, objeto in enumerate(lista):
+        print(f"{posicao + 1} - {objeto}")
+    print(linha())
+    escolha = leiaInt("Sua opcão: ")
+    print(linha())
+    return escolha
 
+    
--- dados_txt/ex115.py
+++ dados_txt/ex115.py
@@ -1,9 +1,22 @@
 #exercicio 115
 from banco import *
 
-nome_entrada = input("Nome: ")
-idade_entrada = int(input("Idade: "))
-nome(nome_entrada)
-idade(idade_entrada)
 
-print(abrir())
+print(lerArquivo())
+
+while True:
+    escolha = opcoes(["Criar Arquivo","Cadastrar Pessoas","Pessoas Cadastradas","Sair"])
+
+
+    if escolha == 1:
+        print(linha())
+        criarArquivo()
+        print(linha())
+    elif escolha == 2:
+        pass
+    elif escolha == 3:
+        print(lerArquivo())
+    elif escolha == 4:
+        break
+    else:
+        print("Opcão invalida. Essa não tem")
